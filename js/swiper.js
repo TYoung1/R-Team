1a0eaa9464a7f6772746e453a947378533910365
TYoung1 <lail_mind@naver.com>
2023-03-31 15:15:44 +0900

    car

---
+++ js/swiper.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 0,
+    slidesPerGroup : 1,
+    centeredSlides: false,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    slidesPerView:1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".mySwiper2", {
+    spaceBetween: 0,
+    slidesPerGroup : 2,
+    centeredSlides: false,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    slidesPerView:4,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".mySwiper3", {
+    spaceBetween: 0,
+    slidesPerGroup : 1,
+    centeredSlides: false,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    slidesPerView:1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
