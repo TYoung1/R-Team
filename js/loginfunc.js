5d5af2f151608052060911f8038884f49544c908
TYoung1 <lail_mind@naver.com>
2023-04-07 16:34:15 +0900

    no notice

---
+++ js/loginfunc.js
@@ -0,0 +1,9 @@
+// 상세 기능
+function guide_sub_login() {
+    var view = document.getElementById('guide');
+    if (view.style.display === 'none') {
+        view.style.display = 'block';
+      } else {
+        view.style.display = 'none';
+      }
+}
